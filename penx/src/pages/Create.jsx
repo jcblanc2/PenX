01fd2aedfcd78cc40610a3b7aeda6b14fa0780c3
jcblanc2 <jclaytonblanc@gmail.com>
2023-11-14 02:40:57 -0500

    Add search bar

--- penx/src/pages/Create.jsx
+++ penx/src/pages/Create.jsx
@@ -33,27 +33,32 @@ const Create = () => {
     }
 
     return (
-        <main className='p-2 my-0 mx-auto max-w-[750px]' >
+        <main className='px-10 my-0' >
             <NavBar label='Publish' handleClick={onPublish} />
 
-            <form className=' mt-16 '>
-                <input
-                    type="title"
-                    placeholder='Title'
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)} />
+            <form className='mt-16 mx-auto max-w-[900px] pb-10'>
+                <div className='flex flex-col gap-2 justify-center items-center pb-5'>
+                    <input
+                        type="title"
+                        placeholder='Title'
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        className='block w-full text-[1.8rem] text-xl text-[#333] font-bold p-4 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none' autoFocus />
 
-                <input
-                    type="text"
-                    placeholder='Subtitle'
-                    value={subTitle}
-                    onChange={(e) => setSubTitle(e.target.value)} />
+                    <input
+                        type="text"
+                        placeholder='Subtitle'
+                        value={subTitle}
+                        onChange={(e) => setSubTitle(e.target.value)}
+                        className='block w-full mt-3 text-[#333] text-lg font-bold p-4 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none' />
 
-                <input
-                    type="file"
-                    name="file"
-                    id="file"
-                    onChange={(e) => setFiles(e.target.files)} />
+                    <input
+                        type="file"
+                        name="file"
+                        id="file"
+                        onChange={(e) => setFiles(e.target.files)}
+                        className='block w-full text-sm text-[#333] border-gray-300 rounded-lg cursor-pointer bg-gray-50  focus:outline-none p-4 border' />
+                </div>
 
                 <ReactQuill
                     value={content}
--- penx/src/pages/Home.jsx
+++ penx/src/pages/Home.jsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 
 const Home = () => {
     const [posts, setPosts] = useState([]);
+    const [keyword, setKeyword] = useState('');
+
 
     useEffect(() => {
         const fetchPots = async () => {
@@ -16,21 +18,45 @@ const Home = () => {
         fetchPots();
     }, []);
 
+    const handleSearch = () => { }
+
     return (
-        <main className="p-2 my-0 mx-auto max-w-[700px]">
+        <main className="p-2 my-0 mx-auto max-w-[900px]">
             <NavBar />
 
-            <div className=" mt-16">
-                {posts.length > 0 && posts.map(post => (
-                    <Article
-                        key={post._id}
-                        id={post._id}
-                        userName={post.author.name}
-                        cover={post.cover}
-                        title={post.title}
-                        content={post.content}
-                        date={post.createdAt} />
-                ))}
+            <form className="pt-14 pb-20 flex justify-end">
+                <div className="relative">
+                    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg className="w-4 h-4 text-gray-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                        </svg>
+                    </div>
+                    <input
+                        type="search"
+                        id="default-search"
+                        className="block w-[300px] p-4 ps-10 text-sm text-[#333] border border-gray-300 rounded-lg bg-gray-50 focus:outline-none"
+                        placeholder="Search"
+                        required
+                        onChange={(e) => setKeyword(e.target.value)}
+                        onKeyDown={handleSearch} />
+                </div>
+            </form>
+
+
+            <div className="">
+                {posts.length > 0 && posts
+                    .filter((post) =>
+                        post.title.toLowerCase().includes(keyword.toLowerCase())
+                    ).map(post => (
+                        <Article
+                            key={post._id}
+                            id={post._id}
+                            userName={post.author.name}
+                            cover={post.cover}
+                            title={post.title}
+                            content={post.content}
+                            date={post.createdAt} />
+                    ))}
             </div>
         </main>
     )
